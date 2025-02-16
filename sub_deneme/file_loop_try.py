17063c2c5b5fc5e175444f0d1202a18a19ca3965
Ugur's homePC <ubayrak@pm.me>
2025-02-16 23:56:00 +0300

    os.popen study

--- sub_deneme/file_loop_try.py
+++ sub_deneme/file_loop_try.py
@@ -2,12 +2,24 @@ from time import sleep
 import os
 
 # Define file name
-file_name = "/home/ugur/logfile.txt"
+file_name = "/home/ugur/german.txt"
 
 # Open file in write mode to start fresh
-with open(file_name, "w") as file:
-    file.write("Start of log file\n")
-    sleep(2)
+# with open(file_name, "w") as file:
+#     file.write("Start of log file\n")
+#     sleep(2)
 
-print("Misson accomplished.")
-print(f"{os.path.abspath(file_name)}")
+# print("Misson accomplished.")
+# print(f"{os.path.abspath(file_name)}")
+
+linecount = int(os.popen(f'wc -l < {file_name}').read().strip())
+
+while linecount > 0:
+    linecount-=1
+    print(f'now it is {linecount}')
+
+
+print("Program exited.")
+
+
+
