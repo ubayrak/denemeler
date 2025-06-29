2e76afac0e6542a279baae1c5e6759530f6f0e7f
Ugur's homePC <ubayrak@pm.me>
2025-06-29 15:23:50 +0300

    29haziran_lastclean

--- app_deneme/file-counter-app/app/file_counter.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#filecount = int(os.popen('find . -maxdepth 1 -type f | wc -l').read().strip())
-
-location = os.popen('pwd').read().strip()
-
-print(location)
--- app_deneme/file_counter.py
+++
@@ -1,6 +0,0 @@
-from time import sleep
-import os
-
-filecount = int(os.popen(f'find . -maxdepth 1 -type f | wc -l').read().strip())
-
-print(filecount)
