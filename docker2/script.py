994f113b1d0cba15da592fec494e84cf75f548e3
Ugur's homePC <ubayrak@pm.me>
2025-07-13 23:45:08 +0300

    dokcker in selenium first example OK!!!!

---
+++ docker2/script.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://example.com")
+print("Title of the page is:", driver.title)
+
+driver.quit()
